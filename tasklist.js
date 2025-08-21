17cbae95dd8b2daf88496f65e9aefe9ca919011b
Nikita <pensilgoneiv@gmaol.com>
2025-08-21 21:31:47 +0300

    добавил функцию чекбокса

--- tasklist.js
+++ tasklist.js
@@ -9,7 +9,17 @@ button.addEventListener('click', () => {
     const li = document.createElement('li');
     const checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
+    checkBox.addEventListener('change', () => {
+        if(checkBox.checked){
+            li.style.textDecoration = 'line-through'; 
+        } else {
+            li.style.textDecoration = 'none' ;
+        }
+    });
     const delButton = document.createElement('button');
+    delButton.addEventListener("click", () => {
+        li.remove();
+    });
     li.textContent = taskText;
     delButton.textContent = 'Удалить';
     list.appendChild(li);
